c6ded6f8849141e004685b047f3d6ca62f69c965
Manojega0311 <113025165+Manojega0311@users.noreply.github.com>
2022-09-07 21:30:19 -0700

    java

---
+++ Arth.java
@@ -0,0 +1,15 @@
+//arithmetic operators +,-,*,/,%
+class Arth
+{
+	public static void main(str[]args)
+	int X,Y,Z;
+	X=10;
+	Y=5;
+	z=X+Y;
+	m=X-Y;
+	n=X*Y;
+	p=X/Y;
+	q=X%Y;
+	system.out.println(z+" "+m+" "+n+" "+p+" "+q);
+}
+}
